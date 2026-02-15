c40f2bdae3b28eecfcb68c0c197535bab682fbf0
Ajax Dong <i@aajax.top>
2026-02-15 11:49:22 +0800

    Fixed visibility on no JS

--- source/script/bootstarp.js
+++ source/script/bootstarp.js
@@ -2,8 +2,4 @@ document.addEventListener('DOMContentLoaded', function() {
   document.body.addEventListener('touchstart', function () {});
   var app = new Nlvi(nlviconfig);
   app.bootstarp();
-
-  document.querySelectorAll('.container-inner, .logo-inner').forEach(function(el) {
-    el.style.display = 'block';
-  });
 });
